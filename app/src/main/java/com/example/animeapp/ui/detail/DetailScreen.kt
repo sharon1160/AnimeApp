c02501644213fff504a7126381bdb000e594bf4e
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-06-12 11:01:03 -0500

    feat: add detail screen

---
+++ app/src/main/java/com/example/animeapp/ui/detail/DetailScreen.kt
@@ -0,0 +1,9 @@
+package com.example.animeapp.ui.detail
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun DetailScreen(url: String) {
+    Text("detail")
+}
