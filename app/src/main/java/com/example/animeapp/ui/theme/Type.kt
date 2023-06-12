31d28289d5fc436f49676f315653260d12e9d883
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-06-12 10:23:22 -0500

    feat: add typography

--- app/src/main/java/com/example/animeapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/animeapp/ui/theme/Type.kt
@@ -1,28 +1,46 @@
 package com.example.animeapp.ui.theme
 
-import androidx.compose.material.Typography
+import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontStyle
+import com.example.animeapp.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+val Roboto = FontFamily(
+    Font(R.font.roboto_black),
+    Font(R.font.roboto_bold, weight = FontWeight.Bold),
+    Font(R.font.roboto_light, weight = FontWeight.Light),
+    Font(R.font.roboto_thin, weight = FontWeight.Thin),
+    Font(R.font.roboto_italic, weight = FontWeight.Normal, FontStyle.Italic),
+)
+
+val typography = Typography(
+    bodyLarge = TextStyle(
+        fontFamily = Roboto,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
     ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
+    headlineLarge = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.Bold,
+        fontSize = 16.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.Light,
+        fontSize = 16.sp
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.Thin,
+        fontSize = 16.sp
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = Roboto,
+        fontStyle = FontStyle.Italic,
+        fontSize = 16.sp
     )
-    */
 )
