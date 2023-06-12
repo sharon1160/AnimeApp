cb954013d15b317c7b00163383daa01abcd489e6
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-06-12 11:01:14 -0500

    feat: add favorites screen

---
+++ app/src/main/java/com/example/animeapp/ui/favorites/FavoritesScreen.kt
@@ -0,0 +1,10 @@
+package com.example.animeapp.ui.favorites
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun FavoritesScreen(navController: NavHostController) {
+    Text("Fav")
+}
