c9f900b92a94ef3cac808cb2f6b63835c19afe01
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-06-12 11:01:49 -0500

    feat: add home navigation graph

---
+++ app/src/main/java/com/example/animeapp/ui/navigation/graphs/HomeNavGraph.kt
@@ -0,0 +1,38 @@
+package com.example.animeapp.ui.navigation.graphs
+
+import android.net.Uri
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.example.animeapp.ui.navigation.BottomBarItem
+import com.example.animeapp.ui.detail.DetailScreen
+import com.example.animeapp.ui.favorites.FavoritesScreen
+import com.example.animeapp.ui.search.SearchScreen
+
+@Composable
+fun HomeNavGraph(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        route = Graph.HOME,
+        startDestination = BottomBarItem.Search.route
+    ) {
+        composable(route = BottomBarItem.Search.route) {
+            SearchScreen(navController = navController)
+        }
+        composable(route = BottomBarItem.Favorites.route) {
+            FavoritesScreen(navController = navController)
+        }
+        composable(route = "detail/{webUrl}",
+            arguments = listOf(
+                navArgument("webUrl") { type = NavType.StringType }
+            )
+        ) { backStackEntry ->
+            val webUrl =
+                Uri.parse(Uri.decode(backStackEntry.arguments?.getString("webUrl"))).toString()
+            DetailScreen(webUrl)
+        }
+    }
+}
