138be8a2533643edf5da0ee995f3968be6ad3dab
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-06-12 11:03:21 -0500

    feat: add navigation compose

--- app/src/main/java/com/example/animeapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/animeapp/ui/MainActivity.kt
@@ -6,10 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.example.animeapp.ui.navigation.graphs.RootNavigationGraph
 import com.example.animeapp.ui.theme.AnimeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +21,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    RootNavigationGraph(navController = rememberNavController())
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    AnimeAppTheme {
-        Greeting("Android")
     }
 }
