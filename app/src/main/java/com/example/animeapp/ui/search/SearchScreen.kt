904c3bbf3e5240f2c2efa7d876a76143aced9d83
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-06-12 11:02:23 -0500

    feat: add search screen

---
+++ app/src/main/java/com/example/animeapp/ui/search/SearchScreen.kt
@@ -0,0 +1,10 @@
+package com.example.animeapp.ui.search
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun SearchScreen(navController: NavHostController) {
+    Text("Hi")
+}
