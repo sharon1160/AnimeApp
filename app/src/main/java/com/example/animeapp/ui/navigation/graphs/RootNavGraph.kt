7ab63abc7b6e0a05a6f8e021121c8c45166cc5d6
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-06-12 11:02:11 -0500

    feat: add root navigation graph

---
+++ app/src/main/java/com/example/animeapp/ui/navigation/graphs/RootNavGraph.kt
@@ -0,0 +1,25 @@
+package com.example.animeapp.ui.navigation.graphs
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.animeapp.ui.home.HomeScreen
+
+@Composable
+fun RootNavigationGraph(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        route = Graph.ROOT,
+        startDestination = Graph.HOME
+    ) {
+        composable(route = Graph.HOME) {
+            HomeScreen()
+        }
+    }
+}
+
+object Graph {
+    const val ROOT = "root_graph"
+    const val HOME = "home_graph"
+}
