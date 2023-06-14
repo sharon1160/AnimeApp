bf0507d52f91d232bae468ef5342a367ec46426f
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-06-14 10:52:46 -0500

    feat: change sort filter by type filter

--- app/src/main/java/com/example/animeapp/presentation/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/example/animeapp/presentation/ui/search/SearchViewModel.kt
@@ -2,6 +2,7 @@ package com.example.animeapp.presentation.ui.search
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.animeapp.domain.enums.ContentType
 import com.example.animeapp.domain.usecases.GetAnimesUseCase
 import com.example.animeapp.domain.usecases.GetAnimeUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -15,16 +16,29 @@ import javax.inject.Inject
 class SearchViewModel @Inject constructor(
     private val getAnimesUseCase: GetAnimesUseCase,
     private val getAnimeUseCase: GetAnimeUseCase
-): ViewModel() {
+) : ViewModel() {
     private val _uiState = MutableStateFlow(SearchUiState())
     val uiState = _uiState.asStateFlow()
 
-    fun searchAnime(query: String) {
+    fun searchAnime(query: String, typeFilter: ContentType) {
         viewModelScope.launch {
             _uiState.update {
-                it.copy(animes = getAnimesUseCase.execute(search = query))
+                it.copy(
+                    animes = getAnimesUseCase
+                        .execute(
+                            search = query,
+                            type = typeFilter
+                        )
+                )
+            }
+            _uiState.update {
+                it.copy(query = query)
             }
         }
+    }
+
+    fun getTypeFilters(): List<ContentType> {
+        return enumValues<ContentType>().toList()
     }
 
     fun updateQuery(query: String) {
@@ -33,19 +47,16 @@ class SearchViewModel @Inject constructor(
         }
     }
 
-    fun updateSortFilter(sortFilter: String) {
-        _uiState.update {
-            it.copy(sortFilter = sortFilter)
+    fun updateTypeFilter(type: String) {
+        val typeFilter = when (type) {
+            "Anime" -> ContentType.ANIME
+            "Manga" -> ContentType.MANGA
+            else -> {
+                ContentType.ANIME
+            }
         }
-    }
-
-    fun searchAnime(query: String, filter: String = DEFAULT_FILTER) {
         _uiState.update {
-            it.copy(query = query)
+            it.copy(typeFilter = typeFilter)
         }
-    }
-
-    companion object {
-        const val DEFAULT_FILTER = "Filter 1"
     }
 }
