e0e926947d88dd6056b99eb490d13021c068d647
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-06-14 06:43:26 -0500

    feat: add anime mappers

---
+++ app/src/main/java/com/example/animeapp/data/mappers/AnimeMappers.kt
@@ -0,0 +1,44 @@
+package com.example.animeapp.data
+
+import com.example.AnimeQuery
+import com.example.AnimesQuery
+import com.example.animeapp.domain.Character
+import com.example.animeapp.domain.DetailedAnime
+import com.example.animeapp.domain.Anime
+
+fun AnimeQuery.Media.toDomainModel(): DetailedAnime {
+    return DetailedAnime(
+        id = id,
+        coverLargeImage = coverImage?.large ?: "",
+        englishTitle = title?.english ?: "No title",
+        japaneseTitle = title?.native ?: "No title",
+        averageScore = averageScore ?: 0,
+        type = type?.name ?: "",
+        episodes = episodes ?: 0,
+        genres = genres?.mapNotNull { it } ?: emptyList(),
+        description = description ?: "",
+        characters = characters?.edges?.mapNotNull {
+            it?.let { edge ->
+                edge.node?.let { node ->
+                    Character(
+                        id = node.id,
+                        name = node.name?.full ?: "No name",
+                        largeImage = node.image?.large ?: "",
+                        mediumImage = node.image?.medium ?: ""
+                    )
+                }
+            }
+        } ?: emptyList()
+    )
+}
+
+fun AnimesQuery.Medium.toDomainModel(): Anime {
+    return Anime(
+        id = id,
+        coverLargeImage = coverImage?.large ?: "",
+        englishTitle = title?.english ?: "",
+        japaneseTitle = title?.native ?: "",
+        type = type?.name ?: ""
+
+    )
+}
