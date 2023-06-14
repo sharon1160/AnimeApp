30a976681bd43d6f6d357af49f004334eff8a27f
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-06-14 06:48:38 -0500

    feat: move resources

--- app/src/main/java/com/example/animeapp/ui/character/CharacterScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.example.animeapp.ui.character
-
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-
-@Composable
-fun CharacterScreen(){
-    Text(text = "character")
-}
--- app/src/main/java/com/example/animeapp/ui/detail/DetailScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.example.animeapp.ui.detail
-
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-
-@Composable
-fun DetailScreen(url: String) {
-    Text("detail")
-}
