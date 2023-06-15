95a9583a9912a766afb18d0b0ffd7fc252f1f7bb
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-06-15 02:13:11 -0500

    feat: add detailed character data class

---
+++ app/src/main/java/com/example/animeapp/domain/DetailedCharacter.kt
@@ -0,0 +1,10 @@
+package com.example.animeapp.domain
+
+data class DetailedCharacter(
+    val id: Int,
+    val fullName: String,
+    val nativeName: String,
+    val largeImage: String,
+    val gender: String,
+    val description: String
+)
