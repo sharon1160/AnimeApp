fd13ab35d4038b1e03cf9b9142f03aebaa9d445b
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-06-28 16:37:31 -0500

    feat: add domain model mocks

---
+++ app/src/test/java/com/example/animeapp/mocks/DomainModelMocks.kt
@@ -0,0 +1,39 @@
+package com.example.animeapp.mocks
+
+import com.example.animeapp.domain.Anime
+import com.example.animeapp.domain.DetailedAnime
+import com.example.animeapp.domain.DetailedCharacter
+
+object DomainModelMocks {
+    val detailedAnimeMock = DetailedAnime(
+        id = 0,
+        coverLargeImage = "coverLargeImage",
+        englishTitle = "englishTitle",
+        japaneseTitle = "japaneseTitle",
+        type = "type",
+        averageScore = 0,
+        episodes = 0,
+        genres = emptyList(),
+        description = "description",
+        characters = emptyList()
+    )
+
+    val animesListMock = listOf(
+        Anime(
+            id = 0,
+            coverLargeImage = "coverLargeImage",
+            englishTitle = "englishTitle",
+            japaneseTitle = "japaneseTitle",
+            type = "type"
+        )
+    )
+
+    val detailedCharacterMock = DetailedCharacter(
+        id = 0,
+        fullName = "fullName",
+        nativeName = "nativeName",
+        largeImage = "largeImage",
+        gender = "gender",
+        description = "description"
+    )
+}
