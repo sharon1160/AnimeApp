2d1bb10982884770b17d8415d4485e11dfb4b700
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-07-04 11:49:24 -0500

    feat: add new model mocks

--- app/src/androidTest/java/com/example/animeapp/mocks/ModelMocks.kt
+++ app/src/androidTest/java/com/example/animeapp/mocks/ModelMocks.kt
@@ -1,8 +1,29 @@
 package com.example.animeapp.mocks
 
+import com.example.animeapp.domain.Character
+import com.example.animeapp.domain.DetailedAnime
 import com.example.animeapp.domain.DetailedCharacter
 
 object ModelMocks {
+    val detailedAnime = DetailedAnime(
+        id = 1,
+        coverLargeImage = "coverLargeImage",
+        englishTitle = "englishTitle",
+        japaneseTitle = "japaneseTitle",
+        type = "type",
+        averageScore = 1,
+        episodes = 1,
+        genres = listOf("genres"),
+        description = "description",
+        characters = listOf(
+            Character(
+                id = 1,
+                name = "name",
+                largeImage = "largeImage",
+                mediumImage = "mediumImage",
+            )
+        )
+    )
     val detailedCharacter = DetailedCharacter(
         id = 1,
         fullName = "fullName",
