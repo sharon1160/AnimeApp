9684f14b7074e2411bf74c70636364354e4a743a
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-07-04 11:18:22 -0500

    feat: add new test tags

--- app/src/main/java/com/example/animeapp/core/util/TestTags.kt
+++ app/src/main/java/com/example/animeapp/core/util/TestTags.kt
@@ -2,4 +2,7 @@ package com.example.animeapp.core.util
 
 object TestTags {
     const val CHARACTER_CONTENT_SCREEN = "CHARACTER_CONTENT_SCREEN"
+    const val FULL_NAME_TEXT = "FULL_NAME_TEXT"
+    const val NATIVE_NAME_TEXT = "NATIVE_NAME_TEXT"
+    const val LABEL_TEXT = "LABEL_TEXT"
 }
